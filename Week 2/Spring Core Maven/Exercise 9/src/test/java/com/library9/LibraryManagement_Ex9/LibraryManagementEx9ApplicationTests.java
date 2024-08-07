0f961e2f30ec0cdc0949b168ec4f318b8bcdd1c7
Gayathri2505 <116957331+Gayathri2505@users.noreply.github.com>
2024-08-07 15:06:28 +0530

    Add files via upload

---
+++ Week 2/Spring Core Maven/Exercise 9/src/test/java/com/library9/LibraryManagement_Ex9/LibraryManagementEx9ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.library9.LibraryManagement_Ex9;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LibraryManagementEx9ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
