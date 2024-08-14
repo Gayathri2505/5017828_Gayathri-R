cff20d258e4f1e06cb9ecaf5a063e5172c4a6c33
Gayathri2505 <116957331+Gayathri2505@users.noreply.github.com>
2024-08-14 16:57:52 +0530

    Add files via upload

---
+++ Week 3/Exercise 01/src/test/java/com/emp/EmployeeManagement/Ex1/EmployeeManagementEx1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.emp.EmployeeManagement.Ex1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmployeeManagementEx1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
---
+++ Week 3/Exercise 02/src/test/java/com/emp/EmployeeManagementEx2/EmployeeManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.emp.EmployeeManagementEx2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmployeeManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
